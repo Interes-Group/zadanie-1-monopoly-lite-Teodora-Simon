84d7bc0d1f6e5b472dd5b3c7d839c32bb2a0d68d
Theodora2000 <xsimont@stuba.sk>
2021-03-23 21:26:32 +0100

    classes 1.4

--- src/main/java/sk/stuba/fei/uim/oop/Board.java
+++ src/main/java/sk/stuba/fei/uim/oop/Board.java
@@ -1,6 +1,7 @@
 package sk.stuba.fei.uim.oop;
 
 import java.util.ArrayList;
+import java.util.Collections;
 /*
 <template Class T>
 class ArrayList
@@ -15,27 +16,93 @@ public class Board
 
     private ArrayList<Field> fields;
     private ArrayList<Player> players;
+    private ArrayList<Integer> cards;
 
   //  (CardField)f.getValue();
 
-    public  Board(int size, int numPlayers)
+    public  Board(int size, int numPlayers, int cardNum)
     {
 
-        fields =  new ArrayList<Field>(size);
-        players = new ArrayList<Player>();
 
+        players = new ArrayList<Player>();
+        initBoard(size);
+        initPlayers(numPlayers);
+        initCards(cardNum);
+
+    }
+
+    private void initPlayers(int numPlayers)
+    {
         for(int i=0; i<numPlayers; i++)
         {
 
             String name;
             float money;
             name = ZKlavesnice.readString("Enter name of player:");
-            money = ZKlavesnice.readInt("Enter the money he got:");
+            money = 1000;
             Player p= new Player(name,money);
             players.add(p);
 
         }
+
     }
 
+    private void initBoard(int size)
+    {
+        fields=new ArrayList<Field>(Collections.nCopies(size, null));
+        System.out.println(fields.size());
+        Field startField= new Field(FieldType.Start);
+        fields.set(0,startField);
+        Field jailField = new Field(FieldType.Jail);
+        fields.set(6,jailField);
+        Field taxField = new Field(FieldType.Tax_payment);
+        fields.set(12,taxField);
+        Field policeField = new Field(FieldType.Police);
+        fields.set(18,policeField);
+
+        for(int i=1;i<24;i+=6){
+            CardField cardField = new CardField(FieldType.Chance);
+            fields.set(i, cardField);
+        }
+
+    }
+
+    private void initCards(int cardNum)
+    {
+        cards=new ArrayList<Integer>();
+        for(int i=0; i<cardNum; i++)
+        {
+            cards.add(i);
+        }
+        Collections.shuffle(cards);
+
+
+
+    }
+
+
+    public void printPlayers()
+    {
+        for(Player p : players)
+        {
+            System.out.println(p);
+
+        }
+
+    }
+
+    public void printCards()
+    {
+        for(int i=0; i<cards.size(); i++)
+        {
+            System.out.println(cards.get(i));
+        }
+
+    }
+
+    public void printBoard()
+    {
+
+    }
 
 }
